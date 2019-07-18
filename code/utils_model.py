1179d546203d21892121cab37f5f6944cf276b88
ntomita <Naofumi.Tomita.GR@dartmouth.edu>
2019-07-18 14:00:36 -0400

    style: inconsistent indentation convert tabs to 4 spaces (PEP8)

--- code/utils_model.py
+++ code/utils_model.py
@@ -110,50 +110,50 @@ def create_model(num_layers, pretrain):
 
 #get the data transforms:
 def get_data_transforms():
-	
-	data_transforms = {
-	    'train': transforms.Compose([
-	        transforms.CenterCrop(config.patch_size),
-	        transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.2),
-	        transforms.RandomHorizontalFlip(),
-	        transforms.RandomVerticalFlip(),
-	        Random90Rotation(),
-	        transforms.ToTensor(),
-	        transforms.Normalize([0.7, 0.6, 0.7], [0.15, 0.15, 0.15]) #mean and standard deviations for lung adenocarcinoma resection slides
-	    ]),
-	    'val': transforms.Compose([
-	        transforms.CenterCrop(config.patch_size),
-	        transforms.ToTensor(),
-	        transforms.Normalize([0.7, 0.6, 0.7], [0.15, 0.15, 0.15])
-	    ]),
-	    'unnormalize': transforms.Compose([
-	        transforms.Normalize([1/0.15, 1/0.15, 1/0.15], [1/0.15, 1/0.15, 1/0.15])
-	    ]),
-	}
+    
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.CenterCrop(config.patch_size),
+            transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.2),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            Random90Rotation(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.7, 0.6, 0.7], [0.15, 0.15, 0.15]) #mean and standard deviations for lung adenocarcinoma resection slides
+        ]),
+        'val': transforms.Compose([
+            transforms.CenterCrop(config.patch_size),
+            transforms.ToTensor(),
+            transforms.Normalize([0.7, 0.6, 0.7], [0.15, 0.15, 0.15])
+        ]),
+        'unnormalize': transforms.Compose([
+            transforms.Normalize([1/0.15, 1/0.15, 1/0.15], [1/0.15, 1/0.15, 1/0.15])
+        ]),
+    }
 
-	return data_transforms
+    return data_transforms
 
 #printing the model
 def print_params(train_folder, num_epochs, num_layers, 
-				learning_rate, batch_size, 
-				weight_decay, learning_rate_decay, 
-				resume_checkpoint, resume_checkpoint_path, 
-				save_interval, checkpoints_folder, 
-				pretrain, log_csv):
+                learning_rate, batch_size, 
+                weight_decay, learning_rate_decay, 
+                resume_checkpoint, resume_checkpoint_path, 
+                save_interval, checkpoints_folder, 
+                pretrain, log_csv):
 
-	print("train_folder:", train_folder)
-	print("num_epochs:", num_epochs)
-	print("num_layers:", num_layers)
-	print("learning_rate:", learning_rate)
-	print("batch_size:", batch_size)
-	print("weight_decay:", weight_decay)
-	print("resume_checkpoint:", resume_checkpoint)
-	print("resume_checkpoint_path (only if resume_checkpoint is true):", resume_checkpoint_path)
-	print("save_interval:", save_interval)
-	print("output in checkpoints_folder:", checkpoints_folder)
-	print("pretrain:", pretrain)
-	print("log_csv:", log_csv)
-	print()
+    print("train_folder:", train_folder)
+    print("num_epochs:", num_epochs)
+    print("num_layers:", num_layers)
+    print("learning_rate:", learning_rate)
+    print("batch_size:", batch_size)
+    print("weight_decay:", weight_decay)
+    print("resume_checkpoint:", resume_checkpoint)
+    print("resume_checkpoint_path (only if resume_checkpoint is true):", resume_checkpoint_path)
+    print("save_interval:", save_interval)
+    print("output in checkpoints_folder:", checkpoints_folder)
+    print("pretrain:", pretrain)
+    print("log_csv:", log_csv)
+    print()
 
 
 ###########################################
@@ -250,7 +250,7 @@ def train_helper(model, dataloaders, device, dataset_sizes, criterion, optimizer
 
         writer.write('{},{:4f},{:4f},{:4f},{:4f}\n'.format(str(epoch), train_loss, train_acc, val_loss, val_acc))
 
-		#remaining diagnostics
+        #remaining diagnostics
         print('Epoch {} with lr {:.15f}: t_loss: {:.4f} t_acc: {:.4f} v_loss:{:.4f} v_acc: {:.4f}'.format(str(epoch), current_lr, train_loss, train_acc, val_loss, val_acc))
         print()
 
@@ -261,77 +261,77 @@ def train_helper(model, dataloaders, device, dataset_sizes, criterion, optimizer
 
 #main function for training resnet
 def train_resnet(train_folder, num_epochs, num_layers, 
-				learning_rate, batch_size, 
-				weight_decay, learning_rate_decay, 
-				resume_checkpoint, resume_checkpoint_path, 
-				save_interval, checkpoints_folder, 
-				pretrain, log_csv):
+                learning_rate, batch_size, 
+                weight_decay, learning_rate_decay, 
+                resume_checkpoint, resume_checkpoint_path, 
+                save_interval, checkpoints_folder, 
+                pretrain, log_csv):
 
-	#loading in the data
-	data_transforms = {
-	    'train': transforms.Compose([
-	        transforms.CenterCrop(config.patch_size),
-	        transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.2),
-	        transforms.RandomHorizontalFlip(),
-	        transforms.RandomVerticalFlip(),
-	        Random90Rotation(),
-	        transforms.ToTensor(),
-	        transforms.Normalize([0.7, 0.6, 0.7], [0.15, 0.15, 0.15]) #mean and standard deviations for lung adenocarcinoma resection slides
-	    ]),
-	    'val': transforms.Compose([
-	        transforms.CenterCrop(config.patch_size),
-	        transforms.ToTensor(),
-	        transforms.Normalize([0.7, 0.6, 0.7], [0.15, 0.15, 0.15])
-	    ]),
-	    'unnormalize': transforms.Compose([
-	        transforms.Normalize([1/0.15, 1/0.15, 1/0.15], [1/0.15, 1/0.15, 1/0.15])
-	    ]),
-	}
+    #loading in the data
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.CenterCrop(config.patch_size),
+            transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.2),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            Random90Rotation(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.7, 0.6, 0.7], [0.15, 0.15, 0.15]) #mean and standard deviations for lung adenocarcinoma resection slides
+        ]),
+        'val': transforms.Compose([
+            transforms.CenterCrop(config.patch_size),
+            transforms.ToTensor(),
+            transforms.Normalize([0.7, 0.6, 0.7], [0.15, 0.15, 0.15])
+        ]),
+        'unnormalize': transforms.Compose([
+            transforms.Normalize([1/0.15, 1/0.15, 1/0.15], [1/0.15, 1/0.15, 1/0.15])
+        ]),
+    }
 
-	image_datasets = {x: datasets.ImageFolder(os.path.join(config.train_folder, x), data_transforms[x]) for x in ['train', 'val']}
-	dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=x=='train', num_workers=4) for x in ['train', 'val']}
-	dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+    image_datasets = {x: datasets.ImageFolder(os.path.join(config.train_folder, x), data_transforms[x]) for x in ['train', 'val']}
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=x=='train', num_workers=4) for x in ['train', 'val']}
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
 
-	print(len(config.classes), 'classes:', config.classes)
-	print('num train images', len(dataloaders['train'])*batch_size)
-	print('num val images', len(dataloaders['val'])*batch_size)
-	print("CUDA is_available:", torch.cuda.is_available())
-	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(len(config.classes), 'classes:', config.classes)
+    print('num train images', len(dataloaders['train'])*batch_size)
+    print('num val images', len(dataloaders['val'])*batch_size)
+    print("CUDA is_available:", torch.cuda.is_available())
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-	#initialize model
-	if resume_checkpoint == True:
-		model = torch.load(resume_checkpoint_path)
-		print('model loaded from', resume_checkpoint_path)
-	else:
-		model = create_model(num_layers, pretrain)
+    #initialize model
+    if resume_checkpoint == True:
+        model = torch.load(resume_checkpoint_path)
+        print('model loaded from', resume_checkpoint_path)
+    else:
+        model = create_model(num_layers, pretrain)
 
-	model = model.to(device) #same as model.cuda()
+    model = model.to(device) #same as model.cuda()
 
-	#multi class cross entropy
-	criterion = nn.CrossEntropyLoss()
+    #multi class cross entropy
+    criterion = nn.CrossEntropyLoss()
 
-	#adam optimizer
-	optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    #adam optimizer
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
-	#learning rate: exponential, can also try scheduler = lr_scheduler.ReduceLROnPlateau(optimizer)
-	scheduler = lr_scheduler.ExponentialLR(optimizer, gamma=learning_rate_decay)
+    #learning rate: exponential, can also try scheduler = lr_scheduler.ReduceLROnPlateau(optimizer)
+    scheduler = lr_scheduler.ExponentialLR(optimizer, gamma=learning_rate_decay)
 
-	#logging the model after every epoch
-	confirm_output_folder(basefolder(log_csv))
-	writer = open(log_csv, 'w')
-	writer.write('epoch,train_loss,train_acc,val_loss,val_acc\n')
+    #logging the model after every epoch
+    confirm_output_folder(basefolder(log_csv))
+    writer = open(log_csv, 'w')
+    writer.write('epoch,train_loss,train_acc,val_loss,val_acc\n')
 
-	#print
-	print_params(train_folder, num_epochs, num_layers, 
-				learning_rate, batch_size, 
-				weight_decay, learning_rate_decay, 
-				resume_checkpoint, resume_checkpoint_path, 
-				save_interval, checkpoints_folder, 
-				pretrain, log_csv)
+    #print
+    print_params(train_folder, num_epochs, num_layers, 
+                learning_rate, batch_size, 
+                weight_decay, learning_rate_decay, 
+                resume_checkpoint, resume_checkpoint_path, 
+                save_interval, checkpoints_folder, 
+                pretrain, log_csv)
 
 
-	#train model
-	model = train_helper(model, dataloaders, device, dataset_sizes, criterion, optimizer, scheduler, num_epochs, save_interval, writer)
+    #train model
+    model = train_helper(model, dataloaders, device, dataset_sizes, criterion, optimizer, scheduler, num_epochs, save_interval, writer)
 
 
 
@@ -341,106 +341,106 @@ def train_resnet(train_folder, num_epochs, num_layers,
 
 #parsing the validation accuracy from filename
 def parse_val_acc(model_path):
-	no_extension = ".".join(basename(model_path).split('.')[:-1])
-	val_acc = float(no_extension.split('_')[-1][2:])
-	return val_acc
+    no_extension = ".".join(basename(model_path).split('.')[:-1])
+    val_acc = float(no_extension.split('_')[-1][2:])
+    return val_acc
 
 #return the model with the best validation accuracy
 def get_best_model(checkpoints_folder):
-	models = get_image_paths(checkpoints_folder)
-	model_to_val_acc = {model: parse_val_acc(model) for model in models}
-	best_model = max(model_to_val_acc.items(), key=operator.itemgetter(1))[0]
-	return best_model
+    models = get_image_paths(checkpoints_folder)
+    model_to_val_acc = {model: parse_val_acc(model) for model in models}
+    best_model = max(model_to_val_acc.items(), key=operator.itemgetter(1))[0]
+    return best_model
 
 #main function for running on all the generated windows
 def get_predictions(patches_eval_folder, auto_select, eval_model, checkpoints_folder, output_folder):
 
-	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
 
-	#initialize the model
-	model = None
-	if auto_select:
-		model_path = get_best_model(checkpoints_folder)
-	else:
-		model_path = eval_model
+    #initialize the model
+    model = None
+    if auto_select:
+        model_path = get_best_model(checkpoints_folder)
+    else:
+        model_path = eval_model
 
-	model = torch.load(model_path)
-	model.train(False) 
-	print('model loaded from', model_path)
+    model = torch.load(model_path)
+    model.train(False) 
+    print('model loaded from', model_path)
 
-	#for outputting the predictions
-	class_num_to_class = {i:config.classes[i] for i in range(len(config.classes))}
-	class_to_class_num = {v: k for k, v in class_num_to_class.items()}
+    #for outputting the predictions
+    class_num_to_class = {i:config.classes[i] for i in range(len(config.classes))}
+    class_to_class_num = {v: k for k, v in class_num_to_class.items()}
 
-	#data transforms, no augmentation this time.
-	data_transforms = {
-		'normalize': transforms.Compose([
-	        transforms.CenterCrop(224),
-	        transforms.ToTensor(),
-	        transforms.Normalize([0.7, 0.6, 0.7], [0.15, 0.15, 0.15])
-	    ]),
-	    'unnormalize': transforms.Compose([
-	        transforms.Normalize([1/0.15, 1/0.15, 1/0.15], [1/0.15, 1/0.15, 1/0.15])
-	    ]),
-	}
+    #data transforms, no augmentation this time.
+    data_transforms = {
+        'normalize': transforms.Compose([
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.7, 0.6, 0.7], [0.15, 0.15, 0.15])
+        ]),
+        'unnormalize': transforms.Compose([
+            transforms.Normalize([1/0.15, 1/0.15, 1/0.15], [1/0.15, 1/0.15, 1/0.15])
+        ]),
+    }
 
-	start = time.time()
+    start = time.time()
 
-	#load data for each folder:
-	image_folders = get_subfolder_paths(patches_eval_folder) 
-	
-	for image_folder in image_folders: #for each whole slide
+    #load data for each folder:
+    image_folders = get_subfolder_paths(patches_eval_folder) 
+    
+    for image_folder in image_folders: #for each whole slide
 
-		#where we want to write out the predictions
-		confirm_output_folder(output_folder)
-		csv_path = join(output_folder, image_folder.split('/')[-1])+'.csv'
-		writer = open(csv_path, 'w')
-		writer.write("x,y,prediction,confidence\n")
+        #where we want to write out the predictions
+        confirm_output_folder(output_folder)
+        csv_path = join(output_folder, image_folder.split('/')[-1])+'.csv'
+        writer = open(csv_path, 'w')
+        writer.write("x,y,prediction,confidence\n")
 
-		#load the image dataset
-		image_dataset = datasets.ImageFolder(image_folder, data_transforms['normalize'])
-		dataloader = torch.utils.data.DataLoader(image_dataset, batch_size=config.batch_size, shuffle=False, num_workers=4)
-		num_test_image_windows = len(dataloader)*config.batch_size
+        #load the image dataset
+        image_dataset = datasets.ImageFolder(image_folder, data_transforms['normalize'])
+        dataloader = torch.utils.data.DataLoader(image_dataset, batch_size=config.batch_size, shuffle=False, num_workers=4)
+        num_test_image_windows = len(dataloader)*config.batch_size
 
-		#load the image names so we know the coordinates of the windows we are predicting
-		image_folder = join(image_folder, image_folder.split('/')[-1])
-		window_names = get_image_paths(image_folder) 
+        #load the image names so we know the coordinates of the windows we are predicting
+        image_folder = join(image_folder, image_folder.split('/')[-1])
+        window_names = get_image_paths(image_folder) 
 
-		print("testing on", num_test_image_windows, 'crops from', image_folder)
-		batch_num = 0
+        print("testing on", num_test_image_windows, 'crops from', image_folder)
+        batch_num = 0
 
-		#loop through all the windows
-		for test_inputs, test_labels in dataloader:
+        #loop through all the windows
+        for test_inputs, test_labels in dataloader:
 
-			batch_window_names = window_names[batch_num*config.batch_size:batch_num*config.batch_size+config.batch_size]
-			test_inputs = test_inputs.to(device)
-			test_outputs = model(test_inputs)
-			softmax_test_outputs = nn.Softmax()(test_outputs)
-			confidences, test_preds = torch.max(softmax_test_outputs, 1)
+            batch_window_names = window_names[batch_num*config.batch_size:batch_num*config.batch_size+config.batch_size]
+            test_inputs = test_inputs.to(device)
+            test_outputs = model(test_inputs)
+            softmax_test_outputs = nn.Softmax()(test_outputs)
+            confidences, test_preds = torch.max(softmax_test_outputs, 1)
 
-			for i in range(test_preds.shape[0]):
-				#unnormalized_image = data_transforms['unnormalize'](test_inputs[i]).cpu().numpy()
-				#unnormalized_image = np.swapaxes(unnormalized_image, 0, 2) #in case you want to make sure you're looking at the right image
+            for i in range(test_preds.shape[0]):
+                #unnormalized_image = data_transforms['unnormalize'](test_inputs[i]).cpu().numpy()
+                #unnormalized_image = np.swapaxes(unnormalized_image, 0, 2) #in case you want to make sure you're looking at the right image
 
-				#get coordinates and predicted class
-				image_name = batch_window_names[i]
-				x = basename(image_name).split('.')[0].split(';')[0]
-				y = basename(image_name).split('.')[0].split(';')[1]
-				predicted_class = class_num_to_class[test_preds[i].data.item()]
-				confidence = confidences[i].data.item()
+                #get coordinates and predicted class
+                image_name = batch_window_names[i]
+                x = basename(image_name).split('.')[0].split(';')[0]
+                y = basename(image_name).split('.')[0].split(';')[1]
+                predicted_class = class_num_to_class[test_preds[i].data.item()]
+                confidence = confidences[i].data.item()
 
-				#write them 
-				out_line = ','.join([x, y, predicted_class, str(confidence)[:5]]) + '\n'
-				writer.write(out_line)
+                #write them 
+                out_line = ','.join([x, y, predicted_class, str(confidence)[:5]]) + '\n'
+                writer.write(out_line)
 
-				#out_path = 'check_images_2/' + image_folder.split('/')[-1] + '_' + str(counter) + '_' + '_'.join([x, y, predicted_class, str(confidence)[:5]]) + '.jpg'
-				#imsave(out_path, unnormalized_image)
+                #out_path = 'check_images_2/' + image_folder.split('/')[-1] + '_' + str(counter) + '_' + '_'.join([x, y, predicted_class, str(confidence)[:5]]) + '.jpg'
+                #imsave(out_path, unnormalized_image)
 
-			batch_num += 1
+            batch_num += 1
 
-		writer.close()
+        writer.close()
 
-	print('time for', patches_eval_folder, ':', time.time()-start, 'seconds')
+    print('time for', patches_eval_folder, ':', time.time()-start, 'seconds')
 
 
 
