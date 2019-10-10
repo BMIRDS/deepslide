6a18c26b2adf261ec3d5f50c8932d7d4f17e53f0
ntomita <Naofumi.Tomita.GR@dartmouth.edu>
2019-10-10 13:12:29 -0400

    refactor: remove duplicated code, apply yapf Repeated operations are packed in functions in 1_split.py. path handling is generalized. Compliant with PEP8 and yapf.
    *

--- code/6_visualize.py
+++ code/6_visualize.py
@@ -3,16 +3,13 @@
 
 # Visualizing patch predictions with overlaid dots
 
-import utils_evaluation
-from utils_evaluation import *
-
+from utils_evaluation import visualize
 
 # visualize val set
 visualize(wsi_folder=config.wsi_val,
           preds_folder=config.preds_val,
           vis_folder=config.vis_val,
-          colors=config.colors
-          )
+          colors=config.colors)
 
 # visualize test set
 # visualize(wsi_folder=config.wsi_test,
--- code/7_final_test.py
+++ code/7_final_test.py
@@ -3,18 +3,18 @@
 
 # Running the code on the testing set
 
-import utils_evaluation
-from utils_evaluation import *
+from utils_evaluation import (
+    get_best_thresholds,
+    output_all_predictions,
+    print_final_test_results,
+)
 
 best_thresholds = get_best_thresholds(labels_csv=config.labels_val,
-                                      inference_folder=config.inference_val
-                                      )
+                                      inference_folder=config.inference_val)
 
 output_all_predictions(patches_pred_folder=config.preds_test,
                        output_folder=config.inference_test,
-                       conf_thresholds=best_thresholds
-                       )
+                       conf_thresholds=best_thresholds)
 
 print_final_test_results(labels_csv=config.labels_test,
-                         inference_folder=config.inference_test
-                         )
+                         inference_folder=config.inference_test)
